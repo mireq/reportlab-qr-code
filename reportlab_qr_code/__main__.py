b16aec03a5b6ed14f0ec2f41cfc1cd94cfaa353e
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-10-09 16:34:00 +0200

    feat: Added command interface

--- reportlab_qr_code/__main__.py
+++ reportlab_qr_code/__main__.py
@@ -30,6 +30,8 @@ def generate(args):
 	params['radius'] = args.radius
 	if args.enhanced_path is not None:
 		params['enhanced_path'] = args.enhanced_path
+	if args.gradient:
+		params['mask'] = True
 	clean_params(params)
 
 	if isinstance(text, str):
@@ -42,11 +44,104 @@ def generate(args):
 		pageCompression=1 if args.compress else 0
 	)
 	qr.save(c)
+
+	if args.gradient:
+		gradient_type, coords, colors, positions = args.gradient
+		if len(colors) == 1:
+			c.setFillColor(colors[0][1])
+			c.rect(0, 0, qr.size, qr.size, fill=1, stroke=0)
+		else:
+			if gradient_type == 'linear':
+				coords = [
+					coords[0] * qr.size, (1.0 - coords[1]) * qr.size,
+					coords[2] * qr.size, (1.0 - coords[3]) * qr.size,
+				]
+				c.linearGradient(*coords, colors=colors, positions=positions)
+			else:
+				coords = [
+					coords[0] * qr.size, (1.0 - coords[1]) * qr.size,
+					coords[2] * qr.size,
+				]
+				c.radialGradient(*coords, colors=colors, positions=positions)
+
 	c.showPage()
 	c.save()
 
 
+def parse_gradient(val):
+	try:
+		steps = []
+
+		val = val.split()
+		if len(val) == 0:
+			raise ValueError("Invalid format")
+		gradient_type = val[0]
+		coords = []
+		if gradient_type == 'linear':
+			if len(gradient_type) < 5:
+				raise ValueError("Invalid format")
+			coords = val[1:5]
+			val = val[5:]
+		elif gradient_type == 'radial':
+			if len(gradient_type) < 4:
+				raise ValueError("Invalid format")
+			coords = val[1:4]
+			val = val[4:]
+		else:
+			raise ValueError("Invalid format")
+		coords = [float(pos) for pos in coords]
+
+		# Interate over positions and colors
+		position = None
+		for color in val:
+			if color[:1] == '#':
+				steps.append([position, color])
+				position = None
+			else:
+				position = float(color)
+
+		if len(steps) < 1:
+			raise ValueError("Invalid format")
+
+		# Set first and last step if is not set
+		if steps[-1][0] is None:
+			steps[-1][0] = 1
+		if steps[0][0] is None:
+			steps[0][0] = 0
+
+		# Fill gaps
+		last_position = steps[0][0]
+		gap = 0
+		for i, step in enumerate(steps):
+			position = step[0]
+			if position is None:
+				gap += 1
+			elif gap:
+				distance = position - last_position
+				for index in range(i - gap, i):
+					position = last_position + (index - i + gap + 1) * distance / (gap + 1)
+					steps[index][0] = position
+				gap = 0
+			if position is not None:
+				last_position = position
+
+		return (gradient_type, coords, [step[1] for step in steps], [step[0] for step in steps])
+	except Exception as e:
+		sys.stdout.write(str(e))
+		sys.stdout.write('\n')
+		raise
+
+
 def main():
+	gradient_help = """
+Either "linear x1 y1 x2 y2 colors" or "radial x y radius colors" Dimensions are
+in range [0, 1], position (0, 0) is top left corner, (1, 1) is bottomr right
+corner.
+Colors is list "[position] color" e.g. "0.0 #ffffff 1.0 #000000". Position is
+optional. Without position argument, distances are calculated automatically.
+Example: --gradient "linear 0.0 0.0 0.1 1.0 0.5 \#1050c0 0.3 \#1050c0 0.7 \#e0e000"
+	"""
+
 	parser = argparse.ArgumentParser(description="Generate qr code")
 	parser.add_argument('text', nargs='?', type=str, help="Input text or stdin if omitted")
 	parser.add_argument('--outfile', nargs='?', type=argparse.FileType('wb'), default=sys.stdout.buffer, help="Output file or stdout if omitted")
@@ -63,6 +158,8 @@ def main():
 	parser.add_argument('--radius', type=float, help="Round code (radius)", default=0.0)
 	parser.add_argument('--enhanced-path', action='store_true', help="Enhanced path rendering")
 	parser.add_argument('--no-enhanced-path', dest='enhanced_path', action='store_false')
+	parser.add_argument('--gradient', type=parse_gradient, help=gradient_help)
+	parser.set_defaults(compress=True)
 	parser.set_defaults(enhanced_path=None)
 	args = parser.parse_args()
 	generate(args)
