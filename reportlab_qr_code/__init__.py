834db73981008469c8ab97c5bc3817655e9b3c46
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-05-27 14:04:55 +0200

    feat: Added alignment pattern calculation

--- reportlab_qr_code/__init__.py
+++ reportlab_qr_code/__init__.py
@@ -44,6 +44,49 @@ LENGTH_RELATIVE = 2
 PARAMS_GLOBAL = 0
 PARAMS_PART = 1
 
+ALIGN_POSITION_TABLE = [
+	[],
+	[6, 18],
+	[6, 22],
+	[6, 26],
+	[6, 30],
+	[6, 34],
+	[6, 22, 38],
+	[6, 24, 42],
+	[6, 26, 46],
+	[6, 28, 50],
+	[6, 30, 54],
+	[6, 32, 58],
+	[6, 34, 62],
+	[6, 26, 46, 66],
+	[6, 26, 48, 70],
+	[6, 26, 50, 74],
+	[6, 30, 54, 78],
+	[6, 30, 56, 82],
+	[6, 30, 58, 86],
+	[6, 34, 62, 90],
+	[6, 28, 50, 72, 94],
+	[6, 26, 50, 74, 98],
+	[6, 30, 54, 78, 102],
+	[6, 28, 54, 80, 106],
+	[6, 32, 58, 84, 110],
+	[6, 30, 58, 86, 114],
+	[6, 34, 62, 90, 118],
+	[6, 26, 50, 74, 98, 122],
+	[6, 30, 54, 78, 102, 126],
+	[6, 26, 52, 78, 104, 130],
+	[6, 30, 56, 82, 108, 134],
+	[6, 34, 60, 86, 112, 138],
+	[6, 30, 58, 86, 114, 142],
+	[6, 34, 62, 90, 118, 146],
+	[6, 30, 54, 78, 102, 126, 150],
+	[6, 24, 50, 76, 102, 128, 154],
+	[6, 28, 54, 80, 106, 132, 158],
+	[6, 32, 58, 84, 110, 136, 162],
+	[6, 26, 54, 82, 110, 138, 166],
+	[6, 30, 58, 86, 114, 142, 170],
+]
+
 
 @dataclass
 class Length(object):
@@ -443,7 +486,22 @@ class ReportlabImageBase(qrcode.image.base.BaseImage):
 				for x in range(area[0], area[0] + area[2]):
 					for y in range(area[1], area[1] + area[3]):
 						mask_action(x, y)
-		self.bitmap = array.array('B', [bit * mask for bit, mask in zip(self.bitmap, mask)])
+		self.bitmap = mask
+		#self.bitmap = array.array('B', [bit * mask for bit, mask in zip(self.bitmap, mask)])
+
+	def get_version(self):
+		return (self.width - 21) // 4 + 1
+
+	def get_align_positions(self):
+		positions = []
+		version = self.get_version()
+		for x in ALIGN_POSITION_TABLE[version]:
+			for y in ALIGN_POSITION_TABLE[version]:
+				# exclude eyes
+				if x < 8 and y < 8 or x > self.width - 8 and y < 8 or x < 8 and y > self.width - 8:
+					continue
+				positions.append((x, y))
+		return positions
 
 	def get_fragment_area(self, area_name):
 		if area_name == 'all':
@@ -472,6 +530,10 @@ class ReportlabImageBase(qrcode.image.base.BaseImage):
 			return self.get_fragment_area('eyepupil1') + self.get_fragment_area('eyepupil2') + self.get_fragment_area('eyepupil3')
 		elif area_name == 'eyeballs':
 			return self.get_fragment_area('eyeball1') + self.get_fragment_area('eyeball2') + self.get_fragment_area('eyeball3')
+		elif area_name == 'align':
+			version = (self.width - 21) // 4 + 1
+			print(self.get_align_positions())
+			return []
 		raise ValueError(f"Unknown area {area_name}")
 
 	def finish_part(self):
